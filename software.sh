a3e9bacd55c41cfa76916a631bd80b4929c3e311
bakari <tbertin2002@yahoo.fr>
2023-09-07 03:56:55 +0100

    change2

---
+++ software.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Description : Configuration of a server
+#Author : BAKARI sep 2023
+
+
+echo "Installation of packages on redhat base system like centos 7 or redhat 7, and another one for debian base system like Ubuntu"
+echo
+sleep 2
+echo
+echo " installation of net-tools are going to beging, just be patient!"
+echo
+sleep 2
+echo
+yum install net-tools -y
+echo
+sleep 2
+echo
